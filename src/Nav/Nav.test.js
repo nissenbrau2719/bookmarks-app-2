0c4a643894460bdd93292185043ecebd0cff4adb
Paul Nissen <nissenbrau@gmail.com>
2020-02-27 19:16:16 -0700

    remade bookmarks-app to fix npm run issue

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Nav from './Nav';
+import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <BrowserRouter>
-      <App />
+      <Nav />
     </BrowserRouter>,
     div
   );
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+  API_KEY: '$2a$10$0wdIx4SlPmNIFdMYUfSpeu2S/HhghP01wjElCLovQ8D.gZfXbDs5i',
+}
